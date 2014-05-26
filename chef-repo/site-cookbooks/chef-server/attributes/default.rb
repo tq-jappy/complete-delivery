ef3d61fe39a7493f476ef9819830f9c84af727d1
Takumi Endo <tq-ed@v7.com>
2014-05-27 02:01:20 +0900

    bug fix.

--- chef-repo/site-cookbooks/chef-server/attributes/default.rb
+++ chef-repo/site-cookbooks/chef-server/attributes/default.rb
@@ -1,3 +1,3 @@
-node["chef_server"]["name"] = "chef-server"
-node["chef_server"]["ip_addr"] = "192.168.33.10"
-node["chef_server"]["rpm_file"] = "/share/chef-server-11.0.12-1.el6.x86_64.rpm"
+default["chef_server"]["name"] = "chef-server"
+default["chef_server"]["ip_addr"] = "192.168.33.10"
+default["chef_server"]["rpm_file"] = "/share/chef-server-11.0.12-1.el6.x86_64.rpm"
